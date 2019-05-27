aa666fe38541eee20f5f4ba70c310f204b150eb0
Jason Le <jas0nle@yahoo.com>
2019-05-27 20:26:54 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,7 +29,8 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
- (age >= 13 && age <=19) ? "You are a teenager" : "You are not a teenager";
+ (age >= 13 && age <=19) ? 
+ "You are a teenager" : "You are not a teenager";
   
 }
 
